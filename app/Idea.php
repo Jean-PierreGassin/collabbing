bde4edc224b7e9b2467103452252c5f0553f0bc4
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-03-30 13:16:35 +1000

    Create Eloquent relationships

--- app/Idea.php
+++ app/Idea.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Idea extends Model
 {
-    //
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function supporters()
+    {
+        return $this->hasMany(IdeaSupporter::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(IdeaComment::class);
+    }
+
+    public function applicants()
+    {
+        return $this->hasMany(IdeaApplicant::class);
+    }
 }
--- app/IdeaComment.php
+++ app/IdeaComment.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class IdeaComment extends Model
 {
-    //
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    public function idea()
+    {
+        return $this->hasOne(Idea::class);
+    }
 }
