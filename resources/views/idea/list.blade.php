2bbce83ebe9e420fd818cd0379bb69eff189617d
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-03-31 19:27:36 +1000

    Move status/errors outside of cards

--- resources/views/idea/list.blade.php
+++ resources/views/idea/list.blade.php
@@ -4,17 +4,24 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
+                @if (session('status'))
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                @if (session('errors'))
+                    <div class="alert alert-danger">
+                        @foreach (session('errors')->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </div>
+                @endif
+
                 <div class="card">
                     <div class="card-header"></div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                    </div>
+                    <div class="card-body"></div>
                 </div>
             </div>
         </div>
