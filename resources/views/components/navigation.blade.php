3a8aff018061b61498deb963955afd68fa76964c
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-03-31 19:23:36 +1000

    Add links to Dashboard/Ideas

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -3,14 +3,18 @@
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-
+                <li class="nav-item {{ Route::currentRouteName() === 'ideas.index' ? 'active' : '' }}">
+                    <a class="nav-link" href="{{ route('ideas.index') }}">Ideas</a>
+                </li>
             </ul>
 
             <!-- Right Side Of Navbar -->
@@ -21,14 +25,20 @@
                     <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
                 @else
                     <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->first_name }} <span class="caret"></span>
                         </a>
 
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                            <a class="dropdown-item {{ Route::currentRouteName() === 'dashboard' ? 'active' : '' }}"
+                               href="{{ route('dashboard') }}">
+                                Dashboard
+                            </a>
+
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="
+                                event.preventDefault();
+                                document.getElementById('logout-form').submit();">
                                 {{ __('Logout') }}
                             </a>
 
