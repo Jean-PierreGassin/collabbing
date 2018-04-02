8875fb21926f4bdb88b3ba684daa53068076dd11
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-04-02 14:21:51 +1000

    Add 403 Un-Authorized blade

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.errors')
+
+@section('content')
+    <div class="title m-b-md">
+        403
+    </div>
+
+    <div class="title m-b-md">
+        Halt! Who goes there?
+    </div>
+@stop
